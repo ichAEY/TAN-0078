8460d16afe8b16d61b6fe839f7efee3fe36cacb0
tanem <melikyan.mac@gmail.com>
2026-09-20 21:51:57 +0300

    Allow explicit Hair hero decoration on mixed-specialty client

--- app/master-template.tsx
+++ app/master-template.tsx
@@ -1085,7 +1085,7 @@ export default function MasterTemplate() {
           <div
             className="mct-hero-visual"
           >
-            {siteSpecialtyMode === "hair" && site.images.heroDecoration ? (
+            {site.images.heroDecoration ? (
               <div className="mct-master-tools" aria-hidden="true"><img className="mct-master-hero-image" src={site.images.heroDecoration} alt="" /></div>
             ) : null}
             <figure className="dct-hero-portrait">
